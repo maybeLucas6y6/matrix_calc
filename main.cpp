f8bc73ae04245b44e5977a3e81cf2d8688baea03
maybeLucas6y6 <petro.luca2004@gmail.com>
2022-05-17 20:53:14 +0300

    cleaned

--- main.cpp
+++ main.cpp
@@ -1,19 +1,7 @@
 #include <iostream>
+
 using namespace std;
-//can i put my balls in yo jaws
+
 int main() {
-	cout << "balls";
-
-	//luca looool
-	cout<<"I love black men kissing"
-
-
-
-
-	//nibba
-
-	// yes
-	//balls 2
-
 	return 0;
 }
